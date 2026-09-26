a9c412ee7c97c4ab1708ec73bc67e9c40e80e783
Preston Brown <pbrown@brown-house.net>
2026-09-25 23:00:04 -0400

    fix(telemetry): report QIDI Box and OpenAMS as their own AMS types (prestonbrown/helixscreen#1691)
    Both fell to the default arm and were counted as unknown. No test: a two-case string mapping.

--- src/system/telemetry_manager.cpp
+++ src/system/telemetry_manager.cpp
@@ -1926,6 +1926,12 @@ nlohmann::json TelemetryManager::build_hw_ams_section(const helix::PrinterDiscov
     case AmsType::CFS:
         ams["type"] = "cfs";
         break;
+    case AmsType::QIDI_BOX:
+        ams["type"] = "qidi_box";
+        break;
+    case AmsType::OPENAMS:
+        ams["type"] = "openams";
+        break;
     default:
         ams["type"] = "unknown";
         break;
